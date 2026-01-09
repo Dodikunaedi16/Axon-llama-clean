e2baf0ba2f1712d42419612bf008ed102ffe675b
dodikunaedi16 <dodikunaedi46@gmail.com>
2026-01-09 18:20:40 +0700

    first commit

---
+++ docs/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Cursor />
+  <div class="w-full min-h-[100dvh]">
+    <Nav />
+    <main class="h-full">
+      <router-view />
+    </main>
+
+    <!-- <footer class="common-padding size-full"></footer> -->
+  </div>
+</template>
+
+<script setup>
+  import Nav from '@/components/Nav.vue';
+  import Cursor from '@/components/Cursor.vue';
+</script>
